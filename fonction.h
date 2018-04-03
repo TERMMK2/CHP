a2493bfff7282d4d1afdab9281b885050d035497
ptitbgdu80 <35494499+ptitbgdu80@users.noreply.github.com>
2018-04-03 17:56:42 +0200

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -7,7 +7,10 @@ void charge (int N, int Np, int Me, int& i1, int& iN);
 
 int prodMV(int argc, char * argv[], std::vector<std::vector<double> > A, std::vector<double> x);
 
-int prodMVC(int argc, char * argv[], std::vector<double> D1, std::vector<double> D2, std::vector<double> D3, std::vector<double> D4, std::vector<double> D5, std::vector<double> x, int nx, int ny);
+std::vector<double> prodMVC(std::vector<std::vector<double> > Aloc, std::vector<double> x, int nx, int ny);
 
-void Diag_init(int nx, int ny, std::vector<double>& D1, std::vector<double>& D2, std::vector<double>& D3, std::vector<double>& D4, std::vector<double>& D5);
+double dot(std::vector<double> u, std::vector<double> v);
 
+std::vector<double> vectorsplit(std::vector<double> u);
+
+void Diag_init(int nx, int ny, std::vector<std::vector<double> >& A);
