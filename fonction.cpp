6dca15e788df61a3d6db861cf6d1a057f993f01f
ptitbgdu80 <35494499+ptitbgdu80@users.noreply.github.com>
2018-04-03 17:55:26 +0200

    Update fonction.cpp
    Dernière version de prodMVC, upload de verctorsplit et dot

--- fonction.cpp
+++ fonction.cpp
@@ -96,95 +96,207 @@ int prodMV(int argc, char * argv[], std::vector<std::vector<double> > A, std::ve
 
 //-------------------------------------------------------------
 
-int prodMVC(int argc, char * argv[], std::vector<double> D1, std::vector<double> D2, std::vector<double> D3, std::vector<double> D4, std::vector<double> D5, std::vector<double> x, int nx, int ny)
+std::vector<double> prodMVC(std::vector<std::vector<double> > Aloc, std::vector<double> xloc, int nx, int ny)
 {
-  
-  int N;
-  N = x.size();
-  int i1, iN, j1, jN, r;
-  int Me, Np;
-  std::vector<double> y,yloc;
-  y.resize(N); yloc.resize(N);
-
-  
-  for (int i=0; i<N; i++)
-    {
-      y[i] = 0;
-    }
+  int N, i1, iN, Me, Np;
+  N = nx*ny;
+  std::vector<double> x_haut, x_bas, yloc; //x_haut contient les éléments voisins du dessus
+  x_bas.resize(nx);
+  x_haut.resize(nx);
 
   MPI_Status status;
-  MPI_Init(&argc,&argv);
   MPI_Comm_size(MPI_COMM_WORLD, &Np); // get totalnodes
   MPI_Comm_rank(MPI_COMM_WORLD, &Me);
-  
+
   charge(N,Np,Me,i1,iN);
 
-  std::cout << "Je suis le proc " << Me << " je commence à : " << i1 << " et je termine à : " << iN << std::endl;
-
-
-  //Calcul local de Y
-  
-  yloc.resize(iN-i1+1);
-  
-  for (int i=i1; i<iN+1; i++){ 
-
-    if (i<nx){ //La diagonale D1 n'intervient pas dans le calcul
-  
-      if (i==0)
-	yloc[i-i1] = (D3[i]*x[i] + D4[i]*x[i+1]) + D5[i]*x[i+nx];
-
-      else
-      yloc[i-i1] = (D2[i]*x[i-1] + D3[i]*x[i] + D4[i]*x[i+1]) + D5[i]*x[i+nx];
+  if (Me%2 == 0)
+  {
+    if (Me != Np-1)
+    {
+      MPI_Send(&xloc[iN-nx+1],nx,MPI_DOUBLE,Me+1,100,MPI_COMM_WORLD);
     }
-    
-    else if (i>((ny-1)*nx-1)){ //La diagonale D5 n'intervient pas dans le calcul
-
-      if (i==N-1)
-	yloc[i-i1] = D1[i]*x[i-nx] + (D2[i]*x[i-1] + D3[i]*x[i]);
-
-      else
-      yloc[i-i1] = D1[i]*x[i-nx] + (D2[i]*x[i-1] + D3[i]*x[i] + D4[i]*x[i+1]);
+    if (Me != 0)
+    {
+      MPI_Send(&xloc[i1],nx,MPI_DOUBLE,Me-1,100,MPI_COMM_WORLD);
     }
-
-    else
-      yloc[i-i1] = D1[i]*x[i-nx] + (D2[i]*x[i-1] + D3[i]*x[i] + D4[i]*x[i+1]) + D5[i]*x[i+nx];
-      
-  }    
-   
-  
-  if(Me!=0){
-    MPI_Send(&yloc[0],iN-i1+1,MPI_DOUBLE,0,100,MPI_COMM_WORLD);
   }
   else
+  {
+    MPI_Recv(&x_haut[0],nx,MPI_DOUBLE,Me-1,100,MPI_COMM_WORLD, &status);
+    if (Me != Np-1)
     {
-      for (int i=i1; i<iN+1; i++){
-	y[i] = yloc[i-i1];
+      MPI_Recv(&x_bas[0],nx,MPI_DOUBLE,Me+1,100,MPI_COMM_WORLD, &status);
+    }
+  }
+
+  if (Me%2 == 1)
+  {
+    if (Me != Np-1)
+    {
+      MPI_Send(&xloc[iN-nx+1],nx,MPI_DOUBLE,Me+1,100,MPI_COMM_WORLD);
+    }
+    MPI_Send(&xloc[i1],nx,MPI_DOUBLE,Me-1,100,MPI_COMM_WORLD);
+  }
+  else
+  {
+    if (Me != 0)
+    {
+      MPI_Recv(&x_haut[0],nx,MPI_DOUBLE,Me-1,100,MPI_COMM_WORLD, &status);
+    }
+    if (Me != Np-1)
+    {
+      MPI_Recv(&x_bas[0],nx,MPI_DOUBLE,Me+1,100,MPI_COMM_WORLD, &status);
+    }
+  }
+
+  //Calcul local de Y
+
+  yloc.resize(iN-i1+1);
+
+  for (int i =0 ; i < iN-i1+1; i++)
+  {
+    yloc[i] = 0;
+    yloc[i] += Aloc[2][i]*xloc[i];
+    if (Me == 0) //Pas de voisin en haut sur la première ligne
+    {
+      if (i == 0) //pas de voisin à gauche
+      {
+        yloc[i] += Aloc[3][i]*xloc[i+1];
       }
-      for (int he=1; he<Np; he++){
+      else if (i == iN - i1) //voisin à droite dans x_bas
+      {
+        yloc[i] += Aloc[1][i]*xloc[i-1] + Aloc[3][i]*x_bas[0];
+      }
+      else //voisins gauche et droite dans xloc
+      {
+        yloc[i] += Aloc[1][i]*xloc[i-1] + Aloc[3][i]*xloc[i+1];
+      }
 
-	charge(N,Np,he,j1,jN);
 
-	yloc.resize(jN-j1+1);
+      if (i < iN-i1+1-nx) //voisin du bas dans xloc
+      {
+        yloc[i] += Aloc[4][i]*xloc[i+nx];
+      }
+      else //voisin du bas dans x_bas
+      {
+        yloc[i] += Aloc[4][i]*x_bas[i - (iN-i1-nx+1)];
+      }
 
-	MPI_Recv(&yloc[0],jN-j1+1,MPI_DOUBLE,he,100,MPI_COMM_WORLD, &status);
 
-	for (int i=j1; i<jN+1; i++){
-	  y[i] = yloc[i-j1];
-	}
+      if (i >= nx) //voisin du haut dans xloc à partir de la deuxième ligne
+      {
+        yloc[i] += Aloc[0][i]*xloc[i-nx];
       }
     }
 
-  
-  if (Me==0){
-    std::cout<<"Valeur du produit AX ="<<std::endl;
-    for (int i=0; i<N; i++)
+
+    else if (Me == Np-1) //Pas de voisin en bas sur la dernière ligne
+    {
+      if (i == iN - i1) //pas de voisin à droite
       {
-	std::cout << i << " " << y[i] << std::endl;
+        yloc[i] += Aloc[1][i]*xloc[i-1];
       }
+      else if (i == 0) //voisin à gauche dans x_haut
+      {
+        yloc[i] += Aloc[3][i]*xloc[i+1] + Aloc[1][i]*x_haut[nx-1];
+      }
+      else //voisins gauche et droite dans xloc
+      {
+        yloc[i] += Aloc[1][i]*xloc[i-1] + Aloc[3][i]*xloc[i+1];
+      }
+
+
+      if (i < iN-i1+1-nx) //voisin du bas dans xloc
+      {
+        yloc[i] += Aloc[4][i]*xloc[i+nx];
+      }
+
+
+      if (i >= nx) //voisin du haut dans xloc à partir de la deuxième ligne
+      {
+        yloc[i] += Aloc[0][i]*xloc[i-nx];
+      }
+      else //voisin du haut dans x_haut
+      {
+        yloc[i] += Aloc[0][i]*x_haut[i];
+      }
+    }
+
+
+    else //Cas général
+    {
+      if (i == 0) //voisin à gauche dans x_haut
+      {
+        yloc[i] += Aloc[1][i]*x_haut[nx-1] + Aloc[3][i]*xloc[i+1];
+      }
+      else if (i == iN - i1) //voisin à droite dans x_bas
+      {
+        yloc[i] += Aloc[1][i]*xloc[i-1] + Aloc[3][i]*x_bas[0];
+      }
+      else //voisins gauche et droite dans xloc
+      {
+        yloc[i] += Aloc[1][i]*xloc[i-1] + Aloc[3][i]*xloc[i+1];
+      }
+
+
+      if (i < iN-i1+1-nx) //voisin du bas dans xloc
+      {
+        yloc[i] += Aloc[4][i]*xloc[i+nx];
+      }
+      else //voisin du bas dans x_bas
+      {
+        yloc[i] += Aloc[4][i]*x_bas[i - (iN-i1-nx+1)];
+      }
+
+
+      if (i >= nx) //voisin du haut dans xloc
+      {
+        yloc[i] += Aloc[0][i]*xloc[i-nx];
+      }
+      else //voisin du haut dans x_haut
+      {
+        yloc[i] += Aloc[0][i]*x_haut[i];
+      }
+    }
+
+
+  }
+  return yloc;
+}
+
+double dot(std::vector<double> uloc, std::vector<double> vloc)
+{
+  int Nloc = uloc.size();
+  double y,yloc;
+
+  //Calcul d'une partie de y
+  yloc = 0;
+  for (int i=0; i<Nloc; i++)
+  {
+    yloc += uloc[i]*vloc[i];
   }
 
-  MPI_Finalize(); 
-  return 0;
+  MPI_Allreduce(&yloc,&y,1,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
+
+  return y;
+}
+
+std::vector<double> vectorsplit(std::vector<double> u)
+{
+  int N, Np, Me, i1, iN;
+  std::vector<double> uloc;
+  MPI_Status status;
+  MPI_Comm_size(MPI_COMM_WORLD, &Np); // get totalnodes
+  MPI_Comm_rank(MPI_COMM_WORLD, &Me);
+  N = u.size();
+  charge(N,Np,Me,i1,iN);
+  uloc.resize(iN - i1 + 1);
+  for (int i = 0; i <= iN - i1; i++)
+  {
+    uloc[i] = u[i + i1];
+  }
+  return uloc;
 }
 
 
