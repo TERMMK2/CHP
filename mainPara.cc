156505a65250a48ba8f93a196e07805f116c0078
ptitbgdu80 <35494499+ptitbgdu80@users.noreply.github.com>
2018-05-05 00:07:27 +0200

    Update mainPara.cc

--- mainPara.cc
+++ mainPara.cc
@@ -11,9 +11,6 @@
 
 using namespace std;
 
-// COMMENTER UN PEU !
-// GENRE METTRE DES TITRES.
-
 
 int main(int argc, char * argv[])
 {
@@ -37,10 +34,10 @@ int main(int argc, char * argv[])
 
   string CL_bas = "Neumann"; // "Neumann" , "Dirichlet"
   string CL_haut = "Neumann";
-  string CL_gauche = "Neumann_non_constant"; //Peut valoir "Neumann_non_constant"
+  string CL_gauche = "Neumann_non_constant"; //Peut valoir "Neumann_non_constant", cette condition est celle proposée par Mme Baranger dans notre TER, un flux au bord affine par morceaux
   string CL_droite = "Neumann";
 
-  double Val_CL_bas = 0; //Flux si CL_bas == "Neumann", Température si CL_bas == "Dirichlet"
+  double Val_CL_bas = 0; //Il sagit de la valeur du Flux si CL_bas == "Neumann", ou de la Température si CL_bas == "Dirichlet"
   double Val_CL_haut = 0;
   double Val_CL_gauche = 0; 
   double Val_CL_droite = 0;
@@ -50,19 +47,19 @@ int main(int argc, char * argv[])
 
   int nb_iterations = int(ceil(tfinal/deltaT));
 
-  string Source = "non"; //Peut prendre non, polynomial, trigonometriqueou instationnaire. 
+  string Source = "non"; //Peut prendre comme valeur "non", "polynomial", "trigonometrique" ou "instationnaire". 
   //Choisir trigonométrique met à jour les conditions limites automatiquement.
 
   double CI = 293.;
 
   
-  string save_all_file = "TER"; //Mettre non si on ne souhaite pas enregistrer la solution globale au cours du temps sous une forme lisible par paraview
+  string save_all_file = "TER"; //Mettre "non" si on ne souhaite pas enregistrer la solution globale au cours du temps sous une forme lisible par paraview
   
-  string save_points_file = "points_TER"; //Mettre non si l'on 
+  string save_points_file = "points_TER"; //Mettre non si on ne veut pas sauvegarder la température au cours du temps en des points paritculiers 
   int number_saved_points=3;
   vector<vector <double> > saved_points;
   saved_points.resize(number_saved_points);
-  for (int i; i<number_saved_points; i++)
+  for (int i; i<number_saved_points; i++) //Coordonnées des points où on veut sauvegarder la solution au cours du temps, permet d'afficher dans un graphe avec Gnuplot ou pyplot par exemple
     {
       saved_points[i].resize(2);
       saved_points[i][0] = 0.002*i;
@@ -73,7 +70,7 @@ int main(int argc, char * argv[])
   Laplacian2D *Lap;
   Lap = new EC_ClassiqueP();
 
-
+// Initialisation de toutes les variables, ne pas toucher...
   Lap->Initialize(xmin,xmax,ymin,ymax,Nx,Ny,a,deltaT, Me, Np, Source, save_all_file, save_points_file, number_saved_points, saved_points);
   Lap->InitializeCI(CI);
   Lap->InitializeCL(CL_bas, CL_haut, CL_gauche, CL_droite, Val_CL_bas, Val_CL_haut, Val_CL_gauche, Val_CL_droite);
